c4b8c42a6f87f71ec29b60ed5aeda54572bfd6ef
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-10-08 18:44:24 +0200

    Fix Ports

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -6,16 +6,18 @@ use std::{fmt::format, net::SocketAddr};
 const DB_URL: &str = env!("DATABASE_URL");
 
 // API PORT
-const API_PORT: &str = env!("API_PORT");
+const API_PORT_STR: &str = env!("API_PORT");
 lazy_static! {
-    static ref PORT: u16 = API_PORT.parse().unwrap_or(8080);
+    static ref API_PORT: u16 = API_PORT_STR.parse().unwrap_or_else(|e| {
+        println!("Could not find port {}. Defaulting to 8080", API_PORT_STR);
+        8080
+    });
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(index));
-
-    let address = SocketAddr::from(([0, 0, 0, 0], *PORT));
+    let address = SocketAddr::from(([0, 0, 0, 0], *API_PORT));
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
