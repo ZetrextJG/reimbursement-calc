2ae90e2cc4d3bedb4cbc2e3721b8a438f40fbcbe
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-10-09 02:16:25 +0200

    Experiment with basic topbar

--- svelte-frontend/src/routes/+layout.svelte
+++ svelte-frontend/src/routes/+layout.svelte
@@ -12,9 +12,23 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar class="py-0 m-3">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex flex-row items-center space-x-3 space-y-0">
+					<div class="basis-1/2 drop-shadow-md">
+						<strong class="text-l uppercase">Reimbursement Calc</strong>
+					</div>
+					<spacer type="horizontal" width="2em" height="2em" class="px-5"> ♢ </spacer>
+					<div class="basis-1/12 w-100 bg-inherit hover:bg-gray-700 py-1">
+						<button class="btn text-l w-100"> Home </button>
+					</div>
+					<div class="basis-1/12 w-100 bg-inherit hover:bg-gray-700 py-1">
+						<button class="btn text-l w-100"> History </button>
+					</div>
+					<div class="basis-1/12 w-100 bg-inherit hover:bg-gray-700 py-1">
+						<button class="btn text-l w-100"> Admin </button>
+					</div>
+				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
