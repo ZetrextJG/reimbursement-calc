01c0a43d7272eef1d1b1095830fcf6055ec619a0
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-10-16 18:12:26 +0200

    Add integration tests

---
+++ svelte-frontend/integration.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('test users count fetch', () => {
+  it('it should return a number', async () => {
+    const url = "http://localhost:8080/users/count";
+    const response = await fetch(url);
+    const json = await response.json();
+    const count: Number = json.count;
+    expect(count).toBeGreaterThan(0);
+  });
+});
