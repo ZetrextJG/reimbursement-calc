ea8b776f89f5899f6000436c8691fff26193adfe
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2023-10-22 21:00:15 +0200

    Reinforce config creation. Fix origin

--- axum-backend/src/config.rs
+++ axum-backend/src/config.rs
@@ -1,16 +1,31 @@
 use dotenv_codegen::dotenv;
+use validator::Validate;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validate)]
 pub struct Config {
+    #[validate(range(min = 1, max = 65535, message = "port must be between 1 and 65535"))]
     pub port: i32,
+    #[validate(length(min = 1, message = "database_url cannot be empty"))]
     pub database_url: String,
+    #[validate(length(min = 1, message = "jwt_secret cannot be empty"))]
     pub jwt_secret: String,
+    #[validate(length(min = 1, message = "jwt_expires_in cannot be empty"))]
     pub jwt_expires_in: String,
+    #[validate(range(
+        min = 1,
+        max = 65535,
+        message = "jwt_maxage must be between 1 and 65535"
+    ))]
     pub jwt_maxage: i32,
+    #[validate(length(min = 1, message = "smtp_username cannot be empty"))]
     pub smtp_username: String,
+    #[validate(length(min = 1, message = "smtp_password cannot be empty"))]
     pub smtp_password: String,
+    #[validate(length(min = 1, message = "smtp_host cannot be empty"))]
     pub smtp_host: String,
+    #[validate(range(min = 1, max = 65535))]
     pub smtp_port: u16,
+    #[validate(length(min = 1, message = "frontend_origin cannot be empty"))]
     pub frontend_origin: String,
 }
 
@@ -23,14 +38,14 @@ const SMTP_PORT: &str = dotenv!("SMTP_PORT");
 const FRONTEND_ORIGIN: &str = dotenv!("FRONTEND_ORIGIN");
 
 impl Config {
-    pub fn init() -> Config {
+    pub fn init() -> Result<Config, validator::ValidationErrors> {
         // AT RUN TIME (sensitive)
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let jwt_secret = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
         let smtp_username = std::env::var("SMTP_USERNAME").expect("SMTP_USERNAME must be set");
         let smtp_password = std::env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD must be set");
 
-        Config {
+        let config = Config {
             port: PORT.parse::<i32>().expect("Invalid PORT"),
             database_url,
             jwt_secret,
@@ -41,6 +56,8 @@ impl Config {
             smtp_host: SMTP_HOST.to_owned(),
             smtp_port: SMTP_PORT.parse::<u16>().expect("Invalid SMTP_PORT"),
             frontend_origin: FRONTEND_ORIGIN.to_owned(),
-        }
+        };
+        config.validate()?;
+        return Ok(config);
     }
 }
--- axum-backend/src/main.rs
+++ axum-backend/src/main.rs
@@ -24,7 +24,7 @@ pub struct AppState {
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let _ = dotenv::from_filename(".env.priv");
-    let config = Config::init();
+    let config = Config::init()?;
 
     let pool = PgPoolOptions::new()
         .max_connections(10)
@@ -33,13 +33,10 @@ async fn main() -> Result<(), anyhow::Error> {
 
     sqlx::migrate!("./migrations").run(&pool).await?;
 
-    let origins = vec![
-        config.frontend_origin.parse::<HeaderValue>().unwrap(),
-        "http://127.0.0.1".parse::<HeaderValue>().unwrap(),
-    ];
+    let origin = config.frontend_origin.parse::<HeaderValue>()?;
 
     let cors = CorsLayer::new()
-        .allow_origin(origins)
+        .allow_origin(origin)
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([header::AUTHORIZATION, header::ACCEPT, header::CONTENT_TYPE]);
@@ -48,10 +45,11 @@ async fn main() -> Result<(), anyhow::Error> {
         pool,
         config: config.clone(),
     });
-    let app = create_router(app_state).layer(CorsLayer::very_permissive());
+    let app = create_router(app_state).layer(cors);
 
     let addr = format!("0.0.0.0:{}", config.port);
     println!("Listening at {}", &addr);
+    println!("Frontend origin: {}", &config.frontend_origin);
     axum::Server::bind(&addr.parse()?)
         .serve(app.into_make_service())
         .await?;
